15a9ab10f9f3b570d6912136842d72087942fcfc
Pablo Mayor <pablomayortomillogmail.com>
2025-02-28 14:31:09 +0100

    Update app component title and add route placeholders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //SI estamos en la ruta hola mundo => HolaMundoComponent
+
+  // /admin => AdminComponent
+];
