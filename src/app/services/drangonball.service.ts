43157c44713d9fce76f225fabd01eb8a1e5cf55b
Pablo Mayor <pablomayortomillogmail.com>
2025-03-06 10:08:25 +0100

    Enhance character management by saving to local storage and clean up HTML output

--- src/app/services/drangonball.service.ts
+++ src/app/services/drangonball.service.ts
@@ -1,17 +1,21 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({providedIn: 'root'})
 export class DragonballService {
-
 
   characters = signal<Character[]>([
       {id:1, name:'Goku', power:15000},
       {id:2, name:'Vegeta', power:8500},
     ]);
 
-    addCharacter(newCharacter : Character){
-      this.characters.update((list) => [...list, newCharacter]);
-    }
+  saveToLocalStorage = effect( (() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  }))
+
+
+  addCharacter(newCharacter : Character){
+    this.characters.update((list) => [...list, newCharacter]);
+  }
 
 }
